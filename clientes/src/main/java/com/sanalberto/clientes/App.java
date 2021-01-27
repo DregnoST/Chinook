cedbd8b251b3702ec9ff82af5168f15d32a1104b
alumnoFP <alumnoFP@DESKTOP-DOQTG3D>
2021-01-27 11:11:18 +0100

    Error de System solucionado

--- clientes/src/main/java/com/sanalberto/clientes/App.java
+++ clientes/src/main/java/com/sanalberto/clientes/App.java
@@ -1,9 +1,9 @@
 package com.sanalberto.clientes;
 
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+    
 }
---
+++ clientes/src/main/java/com/sanalberto/clientes/TestSystem.java
@@ -0,0 +1,10 @@
+package com.sanalberto.clientes;
+
+public class TestSystem {
+
+	public static void main(String[] args) {
+		System.out.println();
+
+	}
+
+}
