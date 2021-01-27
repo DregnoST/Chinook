4a60d637d33ed4e25812146dcbf1c226cac8b050
alumnoFP <alumnoFP@DESKTOP-DOQTG3D>
2021-01-27 11:33:43 +0100

    error de classpath parapersistence.xml

--- clientes/src/main/java/com/sanalberto/clientes/TestSystem.java
+++ clientes/src/main/java/com/sanalberto/clientes/TestSystem.java
@@ -1,10 +1,43 @@
 package com.sanalberto.clientes;
 
+import java.math.BigDecimal;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 public class TestSystem {
-
+	
+	private static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("clientes");
 	public static void main(String[] args) {
-		System.out.println();
 
+		getArtist(60);
+		
+		
+		
+		
+		ENTITY_MANAGER_FACTORY.close();
+	}
+	
+	public static void  getArtist(int id) {
+		EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+		String query = "Select a From ArtistManual a where a.artist = :artID";
+		
+		TypedQuery<Artist> tq = em.createQuery(query, Artist.class);
+		tq.setParameter("artID", id);
+		Artist art = null;
+		try {
+			art = tq.getSingleResult();
+			System.out.println("--> Artist: " + art.getName());
+		} catch (Exception e) {
+			System.out.println("Error recuperando el artista");
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
 	}
 
 }
